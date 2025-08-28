f5134aba7e460d084024a43eaa19daa9c99ae0fc
eqramhossain <ekram.fr03@gmail.com>
2025-08-28 21:44:05 +0200

    update

--- include/philo.h
+++ include/philo.h
@@ -6,12 +6,15 @@
 /*   By: ehossain <ehossain@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/07 20:49:12 by ehossain          #+#    #+#             */
-/*   Updated: 2025/08/12 13:39:27 by ehossain         ###   ########.fr       */
+/*   Updated: 2025/08/28 20:00:35 by ehossain         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PHILO_H
 # define PHILO_H
+
+# define ac_limit 5
+# define ac_limit_op 6
 
 # include <limits.h>
 # include <stdio.h>
@@ -19,21 +22,28 @@
 # include <sys/time.h>
 # include <unistd.h>
 
-typedef struct s_philo
+// 1 sec is equal to 1000 milliseconde
+
+typedef struct s_input
 {
-	int	nb_philo;
-	int	time_to_die;
-	int	time_to_eat;
-	int	time_to_sleep;
+	int		nb_philo;
+	int		time_to_die;
+	int		time_to_eat;
+	int		time_to_sleep;
 	int optional; // number_of_times_each_philosopher_must_eat
 
-}		t_philo;
+}			t_input;
+
+typedef struct s_philo
+{
+	t_input	input;
+}			t_philo;
 
 /*  ====================  UTILS  ====================  */
-void	ft_putchar(char c);
-void	ft_putstr(char *str);
-void	ft_putstr_error(char *str);
-void	ft_putnbr(unsigned int nbr);
-int		ft_atoi(char *str);
+void		ft_putchar(char c);
+void		ft_putstr(char *str);
+void		ft_putstr_error(char *str);
+void		ft_putnbr(unsigned int nbr);
+int			ft_atoi(char *str);
 
 #endif
--- src/main.c
+++ src/main.c
@@ -6,33 +6,61 @@
 /*   By: ehossain <ehossain@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/07 20:50:08 by ehossain          #+#    #+#             */
-/*   Updated: 2025/08/12 14:30:06 by ehossain         ###   ########.fr       */
+/*   Updated: 2025/08/28 20:04:39 by ehossain         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
 
-static void	ft_t_philo_init(char **av, t_philo *philo, int option);
+static void	ft_init_t_input(t_input *input, int ac, char **av);
+static void	ft_print_t_input(t_input *input, int ac);
 
 int	main(int ac, char *av[])
 {
-	t_philo	philo;
+	t_input	input;
 
-	if (ac < 5 || ac > 6)
+	if (ac == 5 || ac == 6)
+	{
+		ft_init_t_input(&input, ac, av);
+		ft_print_t_input(&input, ac);
+	}
+	else
 		return (1);
-	if (ac == 5)
-		ft_t_philo_init(av, &philo, 0);
-	if (ac == 6)
-		ft_t_philo_init(av, &philo, 1);
 	return (0);
 }
 
-static void	ft_t_philo_init(char **av, t_philo *philo, int option)
+static void	ft_init_t_input(t_input *input, int ac, char **av)
 {
-	if (option == 1)
+	input->nb_philo = ft_atoi(av[1]);
+	if (input->nb_philo < 1)
+		exit(1);
+	input->time_to_die = ft_atoi(av[2]);
+	if (input->time_to_die <= 0)
+		exit(1);
+	input->time_to_eat = ft_atoi(av[3]);
+	if (input->time_to_eat <= 0)
+		exit(1);
+	input->time_to_sleep = ft_atoi(av[4]);
+	if (input->time_to_sleep <= 0)
+		exit(1);
+	if (ac == 6)
 	{
+		input->optional = ft_atoi(av[5]);
+		if (input->optional <= 0)
+			exit(1);
 	}
 	else
 	{
+		(void)input->optional;
 	}
+}
+
+static void	ft_print_t_input(t_input *input, int ac)
+{
+	printf("nb_philo = %d\n", input->nb_philo);
+	printf("time_to_die = %d\n", input->time_to_die);
+	printf("time_to_eat = %d\n", input->time_to_eat);
+	printf("time_to_sleep = %d\n", input->time_to_sleep);
+	if (ac == 6)
+		printf("optional = %d\n", input->optional);
 }
